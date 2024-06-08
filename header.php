8ffcb83f054496173abf647e6fb68ba6a8e0aba8
ArinAyu17 <arinayu817@gmail.com>
2024-06-08 10:13:07 +0700

    header.php

--- header.php
+++ header.php
@@ -7,14 +7,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<!-- My Css -->
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<link rel="shortcut icon" type="text/css" href="assets/img/homepage/logo.jpg"/>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="shortcut icon" type="text/css" href="img/homepage/logo.jpg"/>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 
 	<script  type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-		<script type="text/javascript" src="assets/js/materialize.min.js"></script>
+		<script type="text/javascript" src="js/materialize.min.js"></script>
 			<script>
 			$(document).ready(function(){
 				$(".carousel.carousel-slider").carousel({
